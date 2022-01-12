560f546e7e2a84164f9c8ac692331428e2c26ade
home <home@WINDOWS-PQ21EPG>
2022-01-12 10:47:10 +0530

    Add project files.

---
+++ WebApplication28/Data/WebApplication28Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication28.Models;
+
+namespace WebApplication28.Data
+{
+    public class WebApplication28Context : DbContext
+    {
+        public WebApplication28Context (DbContextOptions<WebApplication28Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication28.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ WebApplication28/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication28.Models
+{
+    public class Employee
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public int Salary { set; get; }
+    }
+}
